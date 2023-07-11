ab0bbe31ae1d1cf21e2200f7de6057428aeabbf2
Yuri Campos <ygsoares20@gmail.com>
2023-07-11 03:12:07 -0300

    create the board and player classes

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string board[3][3];
+
+    for ( int i = 0; i < 3; i++ ) {
+        for ( int j = 0; j < 3; j++ )
+        {
+
+            board[i][j] = "P ";
+        }
+    }
+
+
+    for ( int i = 0; i < 3; i++ ) {
+        for ( int j = 0; j < 3; j++ )
+        {
+            cout << board[i][j] << " |";
+        }
+        cout << endl;
+        for (int k = 0; k < 3; k++) {
+            cout << "___ ";
+        }
+        cout << endl;
+    }
+
+
 }
